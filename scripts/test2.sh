6be6ec476a39e735a1b37d3be76a395fb57dbbc5
skye rogers <skyero@amazon.com>
2025-06-02 15:40:07 -0700

    edit script

--- scripts/test2.sh
+++ scripts/test2.sh
@@ -7,7 +7,7 @@ JAR_DIR="amazon_kclpy/jars"
 python "${SETUP_SCRIPT}" download_jars && \
 python "${SETUP_SCRIPT}" install && \
 (cd "${JAR_DIR}" && jar xf "${JAR_FILE}") && echo "UNZIPPED JAR" && \
-pwd && python "${SETUP_SCRIPT}" download_more_jars && \
+python "${SETUP_SCRIPT}" download_more_jars && \
 python "${SETUP_SCRIPT}" install_more
 
 if [ $? -eq 0 ]; then
