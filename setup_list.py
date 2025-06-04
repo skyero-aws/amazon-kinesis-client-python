2b6f0a81101a44e9101e255cec1bc1b9228d06a2
skye rogers <skyero@amazon.com>
2025-06-03 18:13:38 -0700

    remove all pom originals from setup_list

--- setup_list.py
+++ setup_list.py
@@ -54,113 +54,6 @@ DEPENDENCY_LIST_FILE = 'multilang_dependency.txt'
 DEPENDENCY_LIST_FILE_PATH = 'amazon_kclpy/jars/pom-sync/multilang_dependencies.txt'
 MANUAL_JAR_PLACEMENT = 'amazon_kclpy/jars/amazon-kinesis-client-3.0.3-SNAPSHOT.jar'
 
-class MavenJarDownloader:
-
-    def __init__(self, on_completion, destdir=JAR_DIRECTORY, packages_file=REMOTE_MAVEN_PACKAGES_FILE):
-        self.on_completion = on_completion
-        self.destdir = destdir
-        self.packages_file = packages_file
-        self.packages = self.parse_packages_from_pom()
-
-    def warning_string(self, missing_jars=[]):
-        s = '''The following jars were not installed because they were not
-present in this package at the time of installation:'''
-        for jar in missing_jars:
-            s += '\n  {jar}'.format(jar=jar)
-        s += '''
-This doesn't affect the rest of the installation, but may make it more
-difficult for you to run the sample app and get started.
-
-You should consider running:
-
-    python setup.py download_jars
-    python setup.py install
-
-Which will download the required jars and rerun the install.
-'''
-        return s
-
-    def parse_packages_from_pom(self):
-        maven_root = ET.parse(self.packages_file).getroot()
-        maven_version = '{http://maven.apache.org/POM/4.0.0}'
-        # dictionary of common package versions encoded in `properties` section
-        properties = {f"${{{child.tag.replace(maven_version, '')}}}": child.text
-                      for child in maven_root.find(f'{maven_version}properties').iter() if 'version' in child.tag}
-
-        packages = []
-        for dep in maven_root.iter(f'{maven_version}dependency'):
-            dependency = []
-            for attr in ['groupId', 'artifactId', 'version']:
-                val = dep.find(maven_version + attr).text
-                if val in properties:
-                    dependency.append(properties[val])
-                else:
-                    dependency.append(val)
-            packages.append(tuple(dependency))
-
-        return packages
-
-    def download_and_check(self):
-        self.download_files()
-        self.on_completion()
-        missing_jars = self.missing_jars()
-        if len(missing_jars) > 0:
-            raise RuntimeError(self.warning_string(missing_jars))
-
-    def package_destination(self, artifact_id, version):
-        return '{artifact_id}-{version}.jar'.format(artifact_id=artifact_id, version=version)
-
-    def missing_jars(self):
-        file_list = [os.path.join(self.destdir, self.package_destination(p[1], p[2])) for p in self.packages]
-        return [f for f in file_list if not os.path.isfile(f)] # The missing files
-
-    def package_url(self, group_id, artifact_id, version):
-        #
-        # Sample url:
-        # https://search.maven.org/remotecontent?filepath=org/apache/httpcomponents/httpclient/4.2/httpclient-4.2.jar
-        # https://repo1.maven.org/maven2/org/apache/httpcomponents/httpclient/4.2/httpclient-4.2.jar
-        #
-        prefix = os.getenv("KCL_MVN_REPO_SEARCH_URL", 'https://repo1.maven.org/maven2/')
-        return '{prefix}{path}/{artifact_id}/{version}/{dest}'.format(
-                                        prefix=prefix,
-                                        path='/'.join(group_id.split('.')),
-                                        artifact_id=artifact_id,
-                                        version=version,
-                                        dest=self.package_destination(artifact_id, version))
-
-    def download_file(self, url, dest):
-        """
-        Downloads a file at the url to the destination.
-        """
-        print('Attempting to retrieve remote jar {url}'.format(url=url))
-        try:
-            response = urlopen(url)
-            if response.getcode() != 200:
-                raise Exception(f"HTTP Status Code: {response.getcode()}")
-
-            os.makedirs(os.path.dirname(dest), exist_ok=True)
-
-            with open(dest, 'wb') as dest_file:
-                shutil.copyfileobj(response, dest_file)
-            print('Saving {url} -> {dest}'.format(url=url, dest=dest))
-
-            if not os.path.exists(dest) or os.path.getsize(dest) == 0:
-                raise Exception("File download appears to have failed - empty or missing file")
-
-        except Exception as e:
-            print('Failed to retrieve 1 {url}: {e}'.format(url=url, e=e))
-            raise
-
-    def download_files(self):
-        for package in self.packages:
-            dest = os.path.join(self.destdir, self.package_destination(package[1], package[2]))
-            if os.path.isfile(dest):
-                print('Skipping download of {dest}'.format(dest=dest))
-            else:
-                url = self.package_url(package[0], package[1], package[2])
-                self.download_file(url, dest)
-
-
 class MavenJarDownloaderList:
 
     def __init__(self, on_completion, destdir=JAR_DIRECTORY, package_list_file=DEPENDENCY_LIST_FILE, package_list_directory=DEPENDENCY_LIST_FILE_PATH):
@@ -261,33 +154,6 @@ Which will download the required jars and rerun the install.
                 url = self.package_url(package[0], package[1], package[2])
                 self.download_file(url, dest)
 
-
-
-class DownloadJarsCommand(Command):
-    description = "Download the jar files needed to run the sample application"
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        """
-        Runs when this command is given to setup.py
-        """
-        downloader = MavenJarDownloader(on_completion=lambda : None)
-        downloader.download_files()
-        print('''
-Now you should run:
-
-    python setup.py install
-
-Which will finish the installation.
-''')
-
-
 class DownloadMoreJarsCommand(Command):
     description = "Download more jar files needed to run the sample application"
     user_options = []
@@ -311,22 +177,6 @@ Now you should run:
 
 Which will finish the installation.
 ''')
-
-
-class InstallThenCheckForJars(install):
-
-    def do_install(self):
-        install.run(self)
-
-    def run(self):
-        """
-        We override the basic install command. First we download jars then
-        we run the basic install then we check whether the jars are present
-        in this package. If they aren't present we warn the user and give
-        them some advice on how to retry getting the jars.
-        """
-        downloader = MavenJarDownloader(self.do_install)
-        downloader.download_and_check()
 
 class InstallThenCheckForMoreJars(install):
 
