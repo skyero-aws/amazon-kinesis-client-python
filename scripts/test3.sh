4f9a01d5b2f6eefdffb51298414fb59b447aefb1
skye rogers <skyero@amazon.com>
2025-06-09 09:52:18 -0700

    change script to pull from single .py file

--- scripts/test3.sh
+++ scripts/test3.sh
@@ -5,8 +5,8 @@ SETUP_SCRIPT_LIST="setup_list.py"
 JAR_FILE="amazon-kinesis-client-multilang-3.0.3-SNAPSHOT.jar"
 JAR_DIR="amazon_kclpy/jars"
 
-python "${SETUP_SCRIPT_POM}" download_jars && \
-python "${SETUP_SCRIPT_POM}" install && \
+python "${SETUP_SCRIPT_LIST}" download_jars && \
+python "${SETUP_SCRIPT_LIST}" install && \
 (cd "${JAR_DIR}" && jar xf "${JAR_FILE}") && echo "UNZIPPED JAR" && \
 python "${SETUP_SCRIPT_LIST}" download_more_jars && \
 python "${SETUP_SCRIPT_LIST}" install_more
