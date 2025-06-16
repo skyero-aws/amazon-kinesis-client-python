f331879e01e902de0fd17dd785717c9753ffdefa
skye rogers <skyero@amazon.com>
2025-06-16 14:24:41 -0700

    check runner_os

--- .github/scripts/start_kcl.sh
+++ .github/scripts/start_kcl.sh
@@ -3,11 +3,12 @@ set -e
 
 set -o pipefail
 
+echo "RUNNER_OS: $RUNNER_OS"
+KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties)
+
 if [[ "$RUNNER_OS" == "macOS-latest" ]]; then
   brew install coreutils
-  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties)
-  gtimeout 90 $KCL_COMMAND 2>&1 | tee kcl_output.log  || [ $? -eq 124 ]
+  gtimeout 60 $KCL_COMMAND 2>&1 | tee kcl_output.log  || [ $? -eq 124 ]
 else
-  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties)
-  timeout 90 $KCL_COMMAND 2>&1 | tee kcl_output.log || [ $? -eq 124 ]
+  timeout 60 $KCL_COMMAND 2>&1 | tee kcl_output.log || [ $? -eq 124 ]
 fi
