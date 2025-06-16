dde31b9df225b2422f365dfb796388b05c0be7b0
skye rogers <skyero@amazon.com>
2025-06-16 14:27:44 -0700

    fix runner os in scripts

--- .github/scripts/start_kcl.sh
+++ .github/scripts/start_kcl.sh
@@ -3,10 +3,9 @@ set -e
 
 set -o pipefail
 
-echo "RUNNER_OS: $RUNNER_OS"
 KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties)
 
-if [[ "$RUNNER_OS" == "macOS-latest" ]]; then
+if [[ "$RUNNER_OS" == "macOS" ]]; then
   brew install coreutils
   gtimeout 60 $KCL_COMMAND 2>&1 | tee kcl_output.log  || [ $? -eq 124 ]
 else
