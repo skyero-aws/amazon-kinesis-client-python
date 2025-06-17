53835e8bb6a8e2fb1ca4f5d4f533cb3408601bf6
skye rogers <skyero@amazon.com>
2025-06-17 09:30:10 -0700

    15 minute run no table cleanup

--- .github/scripts/clean_up_stream_table.sh
+++ .github/scripts/clean_up_stream_table.sh
@@ -2,13 +2,13 @@
 
 aws kinesis delete-stream --stream-name $STREAM_NAME || true
 
-for i in {1..3}; do
-  aws dynamodb delete-table --table-name $APP_NAME && break ||
-  echo "Retrying DynamoDB Table deletion in 10s" && sleep 10
-done
-for SUFFIX in "-CoordinatorState" "-WorkerMetricStats" "-LeaseManagement"; do
-  if aws dynamodb describe-table --table-name $APP_NAME$SUFFIX &>/dev/null; then
-    echo "Deleting table $APP_NAME$SUFFIX"
-    aws dynamodb delete-table --table-name $APP_NAME$SUFFIX || true
-  fi
-done
+#for i in {1..3}; do
+#  aws dynamodb delete-table --table-name $APP_NAME && break ||
+#  echo "Retrying DynamoDB Table deletion in 10s" && sleep 10
+#done
+#for SUFFIX in "-CoordinatorState" "-WorkerMetricStats" "-LeaseManagement"; do
+#  if aws dynamodb describe-table --table-name $APP_NAME$SUFFIX &>/dev/null; then
+#    echo "Deleting table $APP_NAME$SUFFIX"
+#    aws dynamodb delete-table --table-name $APP_NAME$SUFFIX || true
+#  fi
+#done
