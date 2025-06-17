dfcae4155c7f1814d6ab7fa336e30850225458d9
skye rogers <skyero@amazon.com>
2025-06-17 10:24:21 -0700

    15 min run windows fix

--- .github/scripts/start_kcl.sh
+++ .github/scripts/start_kcl.sh
@@ -2,11 +2,14 @@
 set -e
 set -o pipefail
 
-KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties)
-
-if [[ "$RUNNER_OS" == "macOS" ]]; then
+if [[ "$RUNNER_OS" == "macOS-latest" ]]; then
   brew install coreutils
+  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties)
   gtimeout 900 $KCL_COMMAND 2>&1 | tee kcl_output.log  || [ $? -eq 124 ]
-else
+elif [[ "$RUNNER_OS" == "ubuntu-latest" ]]; then
+  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties)
+  timeout 900 $KCL_COMMAND 2>&1 | tee kcl_output.log || [ $? -eq 124 ]
+elif [[ "$RUNNER_OS" == "windows-latest" ]]; then
+  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --python python --properties samples/sample.properties)
   timeout 900 $KCL_COMMAND 2>&1 | tee kcl_output.log || [ $? -eq 124 ]
 fi
