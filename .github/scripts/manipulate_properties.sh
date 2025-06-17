03bc1d95e0e7199416e08a8c88b8fe9ff3eb8ee0
skye rogers <skyero@amazon.com>
2025-06-17 11:19:11 -0700

    windows fix 12 minute run

--- .github/scripts/manipulate_properties.sh
+++ .github/scripts/manipulate_properties.sh
@@ -7,10 +7,18 @@ if [[ "$RUNNER_OS" == "macOS" ]]; then
   grep -v "idleTimeBetweenReadsInMillis" samples/sample.properties > samples/temp.properties
   echo "idleTimeBetweenReadsInMillis = 250" >> samples/temp.properties
   mv samples/temp.properties samples/sample.properties
-else
+elif [[ "$RUNNER_OS" == "Linux" ]]; then
   sed -i "s/kclpysample/$STREAM_NAME/g" samples/sample.properties
   sed -i "s/PythonKCLSample/$APP_NAME/g" samples/sample.properties
   sed -i "/idleTimeBetweenReadsInMillis/c\idleTimeBetweenReadsInMillis = 250" samples/sample.properties
+elif [[ "$RUNNER_OS" == "Windows" ]]; then
+  sed -i "s/kclpysample/$STREAM_NAME/g" samples/sample.properties
+  sed -i "s/PythonKCLSample/$APP_NAME/g" samples/sample.properties
+  sed -i "/idleTimeBetweenReadsInMillis/c\idleTimeBetweenReadsInMillis = 250" samples/sample.properties
+  sed -i 's/executableName = sample_kclpy_app.py/executableName = python sample_kclpy_app.py/' samples/sample.properties
+else
+  echo "Unknown OS: $RUNNER_OS"
+  exit 1
 fi
 
 cat samples/sample.properties
--- .github/scripts/start_kcl.sh
+++ .github/scripts/start_kcl.sh
@@ -2,29 +2,16 @@
 set -e
 set -o pipefail
 
-#if [[ "$RUNNER_OS" == "macOS" ]]; then
-#  brew install coreutils
-#  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties)
-#  gtimeout 900 $KCL_COMMAND 2>&1 | tee kcl_output.log  || [ $? -eq 124 ]
-#elif [[ "$RUNNER_OS" == "Linux" ]]; then
-#  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties)
-#  timeout 900 $KCL_COMMAND 2>&1 | tee kcl_output.log || [ $? -eq 124 ]
-#elif [[ "$RUNNER_OS" == "Windows" ]]; then
-#  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties)
-#  KCL_COMMAND=${KCL_COMMAND//sample_kclpy_app.py/python sample_kclpy_app.py}
-#  timeout 900 $KCL_COMMAND 2>&1 | tee kcl_output.log || [ $? -eq 124 ]
-#else
-#  echo "Unknown OS: $RUNNER_OS"
-#  exit 1
-#fi
-
 if [[ "$RUNNER_OS" == "macOS" ]]; then
   brew install coreutils
-  amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties
+  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties)
+  gtimeout 720 $KCL_COMMAND 2>&1 | tee kcl_output.log  || [ $? -eq 124 ]
 elif [[ "$RUNNER_OS" == "Linux" ]]; then
-  amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties
+  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties)
+  timeout 720 $KCL_COMMAND 2>&1 | tee kcl_output.log || [ $? -eq 124 ]
 elif [[ "$RUNNER_OS" == "Windows" ]]; then
-  amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties
+  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties)
+  timeout 720 $KCL_COMMAND 2>&1 | tee kcl_output.log || [ $? -eq 124 ]
 else
   echo "Unknown OS: $RUNNER_OS"
   exit 1
