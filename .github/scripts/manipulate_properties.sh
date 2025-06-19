c835f5d0df51919be09eee8bbf47aed606cb4c54
skye rogers <skyero@amazon.com>
2025-06-19 14:24:26 -0700

    change sed method to set last checkpoint to 0

--- .github/scripts/manipulate_properties.sh
+++ .github/scripts/manipulate_properties.sh
@@ -1,37 +1,12 @@
 #!/bin/bash
 set -e
 
-cat > fix_checkpoint.py << 'EOF'
-#!/usr/bin/env python
-import os
+sed -i 's/self._last_checkpoint_time = time.time()/self._last_checkpoint_time = 0/g' samples/sample_kclpy_app.py
 
-# Read the file
-with open('samples/sample_kclpy_app.py', 'r') as f:
-    lines = f.readlines()
-
-# Find and replace the line
-for i, line in enumerate(lines):
-    if 'self._last_checkpoint_time = time.time()' in line:
-        lines[i] = '        self._last_checkpoint_time = 0\n'
-        break
-
-# Write the file back
-with open('samples/sample_kclpy_app.py', 'w') as f:
-    f.writelines(lines)
-
-# Verify the change
-with open('samples/sample_kclpy_app.py', 'r') as f:
-    content = f.read()
-    if 'self._last_checkpoint_time = 0' in content:
-        print("Successfully updated checkpoint time to 0")
-    else:
-        print("Failed to update checkpoint time")
-        exit(1)
-EOF
-
-# Make the script executable and run it
-chmod +x fix_checkpoint.py
-python fix_checkpoint.py
+# For macOS
+if [[ "$RUNNER_OS" == "macOS" ]]; then
+  sed -i '' 's/self._last_checkpoint_time = time.time()/self._last_checkpoint_time = 0/g' samples/sample_kclpy_app.py
+fi
 
 # Manipulate sample.properties file that the KCL application pulls properties from (ex: streamName, applicationName)
 # Depending on the OS, different properties need to be changed
@@ -42,15 +17,17 @@ if [[ "$RUNNER_OS" == "macOS" ]]; then
   grep -v "idleTimeBetweenReadsInMillis" samples/sample.properties > samples/temp.properties
   echo "idleTimeBetweenReadsInMillis = 250" >> samples/temp.properties
   mv samples/temp.properties samples/sample.properties
-#
-#  sed -i "" '/def initialize/,/self._last_checkpoint_time = time.time()/s/self._last_checkpoint_time = time.time()/self._last_checkpoint_time = 0/' samples/sample_kclpy_app.py
+
+  sed -i "" '/def initialize/,/self._largest_seq = (None, None)/s/self._last_checkpoint_time = time.time()/self._last_checkpoint_time = 0/' samples/sample_kclpy_app.py
+
 elif [[ "$RUNNER_OS" == "Linux" ]]; then
   sed -i "s/kclpysample/$STREAM_NAME/g" samples/sample.properties
   sed -i "s/PythonKCLSample/$APP_NAME/g" samples/sample.properties
   sed -i 's/us-east-5/us-east-1/g' samples/sample.properties
   sed -i "/idleTimeBetweenReadsInMillis/c\idleTimeBetweenReadsInMillis = 250" samples/sample.properties
-#
-#  sed -i '/def initialize/,/self._last_checkpoint_time = time.time()/s/self._last_checkpoint_time = time.time()/self._last_checkpoint_time = 0/' samples/sample_kclpy_app.py
+
+  sed -i '/def initialize/,/self._largest_seq = (None, None)/s/self._last_checkpoint_time = time.time()/self._last_checkpoint_time = 0/' samples/sample_kclpy_app.py
+
 elif [[ "$RUNNER_OS" == "Windows" ]]; then
   sed -i "s/kclpysample/$STREAM_NAME/g" samples/sample.properties
   sed -i "s/PythonKCLSample/$APP_NAME/g" samples/sample.properties
@@ -60,8 +37,9 @@ elif [[ "$RUNNER_OS" == "Windows" ]]; then
   echo '@echo off' > samples/run_script.bat
   echo 'python %~dp0\sample_kclpy_app.py %*' >> samples/run_script.bat
   sed -i 's/executableName = sample_kclpy_app.py/executableName = samples\/run_script.bat/' samples/sample.properties
-#
-#  sed -i '/def initialize/,/self._last_checkpoint_time = time.time()/s/self._last_checkpoint_time = time.time()/self._last_checkpoint_time = 0/' samples/sample_kclpy_app.py
+
+  sed -i '/def initialize/,/self._largest_seq = (None, None)/s/self._last_checkpoint_time = time.time()/self._last_checkpoint_time = 0/' samples/sample_kclpy_app.py
+
 else
   echo "Unknown OS: $RUNNER_OS"
   exit 1
