0b0066252984af2840fc85df6ef3530583c1eab1
skye rogers <skyero@amazon.com>
2025-11-13 11:42:05 -0800

    remove DRY and move KCL timeout variable to python.yml

--- .github/scripts/start_kcl.sh
+++ .github/scripts/start_kcl.sh
@@ -5,25 +5,10 @@ set -o pipefail
 chmod +x samples/sample.properties
 chmod +x samples/sample_kclpy_app.py
 
-# Reset the values of checkpoint, leaseCounter, ownerSwitchesSinceCheckpoint, and leaseOwner in DynamoDB table
-#echo "Resetting checkpoint for shardId-000000000000"
-#aws dynamodb update-item \
-#  --table-name $APP_NAME \
-#  --key '{"leaseKey": {"S": "shardId-000000000000"}}' \
-#  --update-expression "SET checkpoint = :checkpoint, leaseCounter = :counter, ownerSwitchesSinceCheckpoint = :switches, leaseOwner = :owner" \
-#  --expression-attribute-values '{
-#    ":checkpoint": {"S": "TRIM_HORIZON"},
-#    ":counter": {"N": "0"},
-#    ":switches": {"N": "0"},
-#    ":owner": {"S": "AVAILABLE"}
-#  }' \
-#  --return-values NONE
-
 # Get records from stream to verify they exist before continuing
 SHARD_ITERATOR=$(aws kinesis get-shard-iterator --stream-name $STREAM_NAME --shard-id shardId-000000000000 --shard-iterator-type TRIM_HORIZON --query 'ShardIterator' --output text)
 INITIAL_RECORDS=$(aws kinesis get-records --shard-iterator $SHARD_ITERATOR)
 RECORD_COUNT_BEFORE=$(echo $INITIAL_RECORDS | jq '.Records | length')
-RUN_TIME_SECONDS=720
 
 echo "Found $RECORD_COUNT_BEFORE records in stream before KCL start"
 
