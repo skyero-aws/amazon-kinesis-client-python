027a476762dc37b3e72e5bbdf75d7ba4ae881b29
skye rogers <skyero@amazon.com>
2025-11-14 10:25:03 -0800

    updated triggers and clean up table retry strategy

--- .github/scripts/clean_up_stream_table.sh
+++ .github/scripts/clean_up_stream_table.sh
@@ -2,28 +2,17 @@
 
 aws kinesis delete-stream --stream-name $STREAM_NAME || true
 
-# Reset the values of checkpoint, leaseCounter, ownerSwitchesSinceCheckpoint, and leaseOwner in DynamoDB table
-#echo "Resetting DDB table"
-#aws dynamodb update-item \
-#  --table-name $APP_NAME \
-#  --key '{"leaseKey": {"S": "shardId-000000000000"}}' \
-#  --update-expression "SET checkpoint = :checkpoint, leaseCounter = :counter, ownerSwitchesSinceCheckpoint = :switches, leaseOwner = :owner" \
-#  --expression-attribute-values '{
-#    ":checkpoint": {"S": "TRIM_HORIZON"},
-#    ":counter": {"N": "0"},
-#    ":switches": {"N": "0"},
-#    ":owner": {"S": "AVAILABLE"}
-#  }' \
-#  --return-values NONE
-
 # Delete all tables
-for i in {1..5}; do
+for i in {1..10}; do
+  echo "Deleting table $APP_NAME"
   aws dynamodb delete-table --table-name $APP_NAME && break ||
-  echo "Retrying DynamoDB Table deletion in 10s" && sleep 10
+  echo "Table deletion failed, attempt $i/10. Retrying DynamoDB Table deletion in $((i * 3)) seconds" && sleep $((i * 3))
 done
-for SUFFIX in "-CoordinatorState" "-WorkerMetricStats" "-LeaseManagement"; do
+for SUFFIX in "-CoordinatorState" "-WorkerMetricStats"; do
   if aws dynamodb describe-table --table-name $APP_NAME$SUFFIX &>/dev/null; then
     echo "Deleting table $APP_NAME$SUFFIX"
-    aws dynamodb delete-table --table-name $APP_NAME$SUFFIX || true
+    for i in {1..10}; do
+      aws dynamodb delete-table --table-name $APP_NAME$SUFFIX && break ||
+      echo "Table deletion failed, attempt $i/10. Retrying DynamoDB Table deletion in $((i * 3))s" && sleep $((i * 3))
   fi
 done
