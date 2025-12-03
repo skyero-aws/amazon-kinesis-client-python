6f70c1e307944820a13c0238f62508bbada373c6
skye rogers <skyero@amazon.com>
2025-12-02 17:12:31 -0800

    robust clean up strategy and added dedicated sample.properties file

--- .github/scripts/start_kcl.sh
+++ .github/scripts/start_kcl.sh
@@ -2,7 +2,7 @@
 set -e
 set -o pipefail
 
-chmod +x samples/sample.properties
+chmod +x .github/resources/sample.properties
 chmod +x samples/sample_kclpy_app.py
 
 # Get records from stream to verify they exist before continuing
@@ -14,10 +14,10 @@ echo "Found $RECORD_COUNT_BEFORE records in stream before KCL start"
 
 if [[ "$RUNNER_OS" == "macOS" ]]; then
   brew install coreutils
-  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties)
+  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties .github/resources/sample.properties)
   gtimeout $RUN_TIME_SECONDS $KCL_COMMAND 2>&1 | tee kcl_output.log  || [ $? -eq 124 ]
 elif [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "Windows" ]]; then
-  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties samples/sample.properties)
+  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties .github/resources/sample.properties)
   timeout $RUN_TIME_SECONDS $KCL_COMMAND 2>&1 | tee kcl_output.log || [ $? -eq 124 ]
 else
   echo "Unknown OS: $RUNNER_OS"
