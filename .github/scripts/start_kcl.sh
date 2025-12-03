a6f89164ffb0fb7ad8f9488a387b44dea88120a4
skye rogers <skyero@amazon.com>
2025-12-02 17:43:06 -0800

    remove redundant records check

--- .github/scripts/start_kcl.sh
+++ .github/scripts/start_kcl.sh
@@ -5,13 +5,6 @@ set -o pipefail
 chmod +x .github/resources/sample.properties
 chmod +x samples/sample_kclpy_app.py
 
-# Get records from stream to verify they exist before continuing
-SHARD_ITERATOR=$(aws kinesis get-shard-iterator --stream-name $STREAM_NAME --shard-id shardId-000000000000 --shard-iterator-type TRIM_HORIZON --query 'ShardIterator' --output text)
-INITIAL_RECORDS=$(aws kinesis get-records --shard-iterator $SHARD_ITERATOR)
-RECORD_COUNT_BEFORE=$(echo $INITIAL_RECORDS | jq '.Records | length')
-
-echo "Found $RECORD_COUNT_BEFORE records in stream before KCL start"
-
 if [[ "$RUNNER_OS" == "macOS" ]]; then
   brew install coreutils
   KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties .github/resources/sample.properties)
