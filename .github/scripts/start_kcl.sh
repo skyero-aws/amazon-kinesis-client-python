c469cd8df8b4452ac305c095c9043acd03447619
skye rogers <skyero@amazon.com>
2025-12-04 15:25:58 -0800

    refactor variables and file names and remove streamArn

--- .github/scripts/start_kcl.sh
+++ .github/scripts/start_kcl.sh
@@ -2,15 +2,15 @@
 set -e
 set -o pipefail
 
-chmod +x .github/resources/sample.properties
+chmod +x .github/resources/github_workflow.properties
 chmod +x samples/sample_kclpy_app.py
 
 if [[ "$RUNNER_OS" == "macOS" ]]; then
   brew install coreutils
-  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties .github/resources/sample.properties)
+  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties .github/resources/github_workflow.properties)
   gtimeout $RUN_TIME_SECONDS $KCL_COMMAND 2>&1 | tee kcl_output.log  || [ $? -eq 124 ]
 elif [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "Windows" ]]; then
-  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties .github/resources/sample.properties)
+  KCL_COMMAND=$(amazon_kclpy_helper.py --print_command --java $(which java) --properties .github/resources/github_workflow.properties)
   timeout $RUN_TIME_SECONDS $KCL_COMMAND 2>&1 | tee kcl_output.log || [ $? -eq 124 ]
 else
   echo "Unknown OS: $RUNNER_OS"
