3f1fd534c60f21c04955dd72ec9d2b8d73c984a5
skye rogers <skyero@amazon.com>
2025-12-04 09:55:01 -0800

    refactor variables and file names and remove streamArn

--- .github/scripts/manipulate_properties.sh
+++ .github/scripts/manipulate_properties.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 set -e
 
-# Manipulate sample.properties file that the KCL application pulls properties from (ex: streamName, applicationName)
+# Manipulate github_workflow.properties file that the KCL application pulls properties from (ex: streamName, applicationName)
 # Depending on the OS, different properties need to be changed
 if [[ "$RUNNER_OS" == "macOS" ]]; then
-  sed -i "" "s/kclpysample/$STREAM_NAME/g" .github/resources/sample.properties
-  sed -i "" "s/PythonKCLSample/$APP_NAME/g" .github/resources/sample.properties
+  sed -i "" "s/STREAM_NAME_PLACEHOLDER/$STREAM_NAME/g" .github/resources/github_workflow.properties
+  sed -i "" "s/APP_NAME_PLACEHOLDER/$APP_NAME/g" .github/resources/github_workflow.properties
 elif [[ "$RUNNER_OS" == "Linux" || "$RUNNER_OS" == "Windows" ]]; then
-  sed -i "s/kclpysample/$STREAM_NAME/g" .github/resources/sample.properties
-  sed -i "s/PythonKCLSample/$APP_NAME/g" .github/resources/sample.properties
+  sed -i "s/STREAM_NAME_PLACEHOLDER/$STREAM_NAME/g" .github/resources/github_workflow.properties
+  sed -i "s/APP_NAME_PLACEHOLDER/$APP_NAME/g" .github/resources/github_workflow.properties
 
   if [[ "$RUNNER_OS" == "Windows" ]]; then
     echo '@echo off' > samples/run_script.bat
     echo 'python %~dp0\sample_kclpy_app.py %*' >> samples/run_script.bat
-    sed -i 's/executableName = sample_kclpy_app.py/executableName = samples\/run_script.bat/' .github/resources/sample.properties
+    sed -i 's/executableName = sample_kclpy_app.py/executableName = samples\/run_script.bat/' .github/resources/github_workflow.properties
   fi
 else
   echo "Unknown OS: $RUNNER_OS"
   exit 1
 fi
 
-cat .github/resources/sample.properties
+cat .github/resources/github_workflow.properties
